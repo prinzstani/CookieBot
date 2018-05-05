cf6bcfa0a5f4dcd66968bfcfbfeedd4fd57d4130
Prinz Stani <andreas@prinz.no>
2018-05-05 20:41:31 +0200

    Make loading of latest stable release possible

--- cookieAutoPlayBeta.js
+++ cookieAutoPlayBeta.js
@@ -1,10 +1,13 @@
 //cookie bot: auto-play-through cookie clicker
-//var xhr = new XMLHttpRequest();
-//xhr.open('GET', 'https://api.github.com/repos/prinzstani/CookieBot/releases/latest', false);
-//xhr.send();
-//var response = JSON.parse(xhr.responseText);
-//var latest = response.html_url + '/cookieAutoPlay.js';
-//latest=latest.replace('github','raw.github');
-//latest=latest.replace('/releases/tag','');
-//Game.LoadMod(latest);
-Game.LoadMod('https://prinzstani.github.io/CookieBot/cookieAutoPlay.js');
+// Here, we just find the last stable release and load it.
+console.log("### COOKIE clicker BETA - trying to link to latest release"); 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/prinzstani/CookieBot/releases/latest', false);
+xhr.send();
+var response = JSON.parse(xhr.responseText);
+var latest = response.html_url + '/cookieAutoPlay.js';
+latest=latest.replace('github','cdn.rawgit');
+latest=latest.replace('/releases/tag','');
+console.log("### LOADING FROM " + latest); 
+Game.LoadMod(latest);
+//Game.LoadMod('https://prinzstani.github.io/CookieBot/cookieAutoPlay.js');
